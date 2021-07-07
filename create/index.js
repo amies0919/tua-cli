1fa35664f5b38254cf8cbfe2bbc363a281b1dae4
shennana <shennana@jd.com>
2021-07-07 11:48:13 +0800

    升级mkdirp

--- create/index.js
+++ create/index.js
@@ -1,9 +1,10 @@
 const path = require("path");
 const fs = require("../utils/fs-promise");
 const install = require("../utils/install");
+const mkdirp = require("mkdirp");
 module.exports = async function(name) {
     const projectDir = path.join(process.cwd(), name);
-    await fs.mkdirp(projectDir);
+    await mkdirp(projectDir);
     console.log(`创建${name}成功`);
     const { template, dir, name: fileName } = require("../generator/package")(name);
     await fs.writeFile(path.join(projectDir, dir, fileName), template.trim());
--- utils/fs-promise.js
+++ utils/fs-promise.js
@@ -1,7 +1,5 @@
 const fs = require("fs");
-const mkdirp = require("mkdirp");
 const promisify = require("./promisify");
 
 exports.writeFile = promisify(fs.writeFile);
 exports.readdir = promisify(fs.readdir);
-exports.mkdirp = promisify(mkdirp);
